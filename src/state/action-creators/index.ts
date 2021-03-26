48887120d8ef4d3db03e0ebb0f9f1c6e804b8634
muhammad-naga <muhammad.naga.ali@gmail.com>
2021-03-26 17:50:18 +0200

    Searching images action creator

---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { Dispatch } from "redux";
+import { ActionTypes } from "../types";
+import { Action } from "./../actions";
+
+const searchImagesCreator = (text: string, page: number) => {
+  return async (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionTypes.SEARCH_IMAGES_REQUEST,
+    });
+
+    try {
+      const CLIENT_ID = "5ekMw6SLOyeT2Yc1v2SGRn7FFpcvhygf-BZQV8zafMY";
+      const { data } = await axios.get(
+        `https://api.unsplash.com/search/photos?client_id=${CLIENT_ID}&page=${page}&query=${text}`
+      );
+
+      const results = data.map((item: any) => item);
+
+      dispatch({
+        type: ActionTypes.SEARCH_IMAGES_SUCCESS,
+        payload: results,
+      });
+    } catch (err) {
+      dispatch({
+        type: ActionTypes.SEARCH_IMAGES_ERROR,
+        payload: err.message,
+      });
+    }
+  };
+};
+
+export default searchImagesCreator;
